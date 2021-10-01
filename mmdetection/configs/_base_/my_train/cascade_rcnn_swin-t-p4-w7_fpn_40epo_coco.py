a0dd655fd411eceafbf40616c8a4ccd8481d7922
YHaJung <hajung3768@gmail.com>
2021-10-01 06:01:59 +0000

    Fix optimizer for SWIN SGD

--- mmdetection/configs/_base_/my_train/cascade_rcnn_swin-t-p4-w7_fpn_40epo_coco.py
+++ mmdetection/configs/_base_/my_train/cascade_rcnn_swin-t-p4-w7_fpn_40epo_coco.py
@@ -30,15 +30,9 @@ optimizer = dict(
     _delete_=True,
     type='AdamW',
     lr=0.0001,
-    #betas=(0.9, 0.999),
     momentum = 0.9,
-    weight_decay=0.05,
-    paramwise_cfg=dict(
-        custom_keys={
-            'absolute_pos_embed': dict(decay_mult=0.),
-            'relative_position_bias_table': dict(decay_mult=0.),
-            'norm': dict(decay_mult=0.)
-        }))
+    weight_decay=0.05
+    )
 lr_config = dict(warmup_iters=1000, step=[8, 11])
 runner = dict(max_epochs=14)
 
